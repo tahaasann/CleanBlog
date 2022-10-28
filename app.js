8ea184f98bcc632b945f117df2105c06fb0ee1ef
Taha S. ASAN <tahaasann@gmail.com>
2022-10-28 16:15:48 +0300

    My First Clean Blog App

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  const photo = {
+    id: 1,
+    title: 'Blog title',
+    description: 'Blog description',
+  };
+  res.send(photo);
+});
+
+const port = 5000;
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı...`);
+});
