0b7b4c7261f83d46eddac84c35b0ba03a874fe53
Taha S. ASAN <tahaasann@gmail.com>
2022-11-02 09:03:06 +0300

    Create Request Body With ExpressJS

--- app.js
+++ app.js
@@ -4,29 +4,32 @@ const path = require('path');
 
 const app = express();
 
-app.set('view engine','ejs');
+//TEMPLATE ENGINE
+app.set('view engine', 'ejs');
 
+//MIDDLEWARES
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-
-
+//ROUTES
 app.get('/home', (req, res) => {
-  
   res.render('index');
 });
 app.get('/add', (req, res) => {
-  
   res.render('add_post');
 });
 app.get('/about', (req, res) => {
-  
   res.render('about');
 });
 app.get('/post', (req, res) => {
-  
   res.render('post');
 });
 
+app.post('/posts', (req, res) => {
+  console.log(req.body);
+  res.redirect('/home');
+});
 
 const port = 5000;
 app.listen(port, () => {
