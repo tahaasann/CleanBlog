2efd0139969611a097aa7145151bdc53732125f7
Taha S. ASAN <tahaasann@gmail.com>
2022-11-03 14:21:59 +0300

    New Update: Photo Details Update is Available

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const ejs = require('ejs');
 const path = require('path');
 const Post = require('./models/Post');
@@ -19,21 +20,22 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(methodOverride('_method'));
 
 //ROUTES
 app.get('/home', async (req, res) => {
   const posts = await Post.find({});
   res.render('index', {
-    posts
+    posts,
   });
 });
 
 app.get('/posts/:id', async (req, res) => {
   const post = await Post.findById(req.params.id);
   res.render('post', {
-    post
+    post,
   });
-})
+});
 
 app.get('/add', (req, res) => {
   res.render('add_post');
@@ -42,12 +44,28 @@ app.get('/about', (req, res) => {
   res.render('about');
 });
 app.get('/post', (req, res) => {
-  res.render('post');
+  res.redirect('/home');
 });
 
 app.post('/posts', async (req, res) => {
   await Post.create(req.body);
   res.redirect('/home');
+});
+
+app.get('/posts/edit/:id', async (req, res) => {
+  const post = await Post.findOne({ _id: req.params.id });
+  res.render('edit', {
+    post,
+  });
+});
+
+app.put('/posts/:id', async (req, res) => {
+  const post = await Post.findOne({ _id: req.params.id });
+  post.title = req.body.title;
+  post.description = req.body.description;
+  post.save();
+
+  res.redirect(`/posts/${req.params.id}`);
 });
 
 const port = 5000;
